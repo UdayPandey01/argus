56c5a3fe4cd15fc6fb8dd6cfddde467432420f30
UdayPandey01 <pandeyuday113@gmail.com>
2026-03-31 23:59:38 +0530

    added ws connection

--- crates/ingestion/src/main.rs
+++ crates/ingestion/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use alloy::providers::{
+    ProviderBuilder,
+    WsConnect
+};
+
+use std::env;
+use dotenvy::dotenv;
+
+#[tokio::main]
+async fn amin() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
+    let alchemy_ws_url = env::var("ALCHEMY_WS_URL").expect("ALCHEMY_WS_URL must be set in .env file");
+
+    println!("Connecting to Ethereum node ")
+
+    
 }
