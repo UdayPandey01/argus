d8cc1dd20119a880533d5ddd365dce4a8755560d
UdayPandey01 <pandeyuday113@gmail.com>
2026-04-01 11:44:52 +0530

    added the decoding of raw evm data

--- crates/ingestion/src/main.rs
+++ crates/ingestion/src/main.rs
@@ -1,10 +1,16 @@
 use alloy::primitives::Address;
 use alloy::providers::{Provider, ProviderBuilder, WsConnect};
 use alloy::rpc::types::Filter;
+use alloy::sol;
 
+use alloy::sol_types::SolEvent;
 use dotenvy::dotenv;
 use futures_util::StreamExt;
 use std::env;
+
+sol! {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -21,7 +27,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let target_address: Address =
         "0xdAC17F958D2ee523a2206206994597C13D831ec7".parse()?;
 
-    let filter = Filter::new().address(target_address);
+    let filter = Filter::new().address(target_address).event_signature(Transfer::SIGNATURE_HASH);
 
     let sub = provider.subscribe_logs(&filter).await?;
     let mut stream = sub.into_stream();
@@ -29,10 +35,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Listening for new events on the USDT contract... (Waiting for a block to be mined)");
 
     while let Some(log) = stream.next().await {
-        println!("NEW EVENT DETECTED!");
-        println!("Block Number: {:?}", log.block_number);
-        println!("Transaction Hash: {:?}", log.transaction_hash);
-        println!("Raw Data: {:?}\n", log.data());
+        match log.log_decode::<Transfer>() {
+            Ok(decode_log) => {
+                let transfer = decode_log.inner;
+
+                println!("USDT TRANSFER DETECTED");
+                println!("TRANSFER FROM: {}", transfer.from);
+                println!("TRANSFER TO: {}", transfer.to);
+
+                println!("RAW AMOUNT: {}", transfer.value);
+                println!("------------------------------------------");
+            }
+            Err(e) => {
+                println!("Failed to decode a log: {:?}", e);
+            }
+        }
     }
 
     Ok(())
